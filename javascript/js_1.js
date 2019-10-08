472b255c0969ba1e149fb4279e6d025c8fb52e63
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-10-08 15:43:00 -0600

    unique in order soliution

---
+++ javascript/js_1.js
@@ -0,0 +1,13 @@
+function uniqueInOrder(it) {
+    var result = []
+    var last
+
+    for (var i = 0; i < it.length; i++) {
+        if (it[i] !== last) {
+            console.log(last);
+            result.push(last = it[i])
+        }
+    }
+
+    return result
+}
